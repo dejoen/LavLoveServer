b8aedb90a38e241d538f8503659cbf3c20f59fe9
dejoen <jaxjoe31@gmail.com>
2024-06-24 20:20:48 +0100

    reg

--- dbConfig/dbConfig.js
+++ dbConfig/dbConfig.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose')
 
 let  initDb = async ()=>{
   
-  return  mongoose.connect(process.env.LOCAL_DB_URL)
+  return  mongoose.connect(process.env.DB_URL)
 }
 
 module.exports= initDb
